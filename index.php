7b0193c2f8116cbcc494862351ddff8b83932325
bande <banasankare@gmail.com>
2018-06-24 20:44:27 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include 'sections/head.php';?>
+<?php include 'sections/navbar.php';?>
+<?php include 'sections/sidebar.php';?>
+<div class="content-wrapper">
+	test
+
+</div>
+<?php include 'sections/footer.php';?>
---
+++ view/ajoutEtudiant.php
@@ -0,0 +1,4 @@
+<?php include'../sections/head.php' ?>
+<?php include'../sections/navbar.php' ?>
+<?php include'../sections/sidebar.php' ?>
+<?php include'../sections/footer.php' ?>
